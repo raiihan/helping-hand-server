573185803d6a6c058842a296570644dad9578d8f
silvaan <s.siilvan@yahoo.com>
2022-04-26 20:28:56 +0600

    event get api created

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ck7gi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const eventCollection = client.db("volunteerHelpingHand").collection("events");
+
+        app.get('/events', async (req, res) => {
+            const query = {};
+            const cursor = eventCollection.find(query);
+            const events = await cursor.toArray();
+            res.send(events)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Volunteer activities is continued')
+})
+app.listen(port, () => {
+    console.log('Listening Port is', port);
+})
