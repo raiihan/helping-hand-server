8669cd6b9f896a94813a13d90cb3b9e49ff7e8f4
silvaan <s.siilvan@yahoo.com>
2022-04-27 13:10:35 +0600

    added route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ck7gi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,19 +18,59 @@ async function run() {
     try {
         await client.connect();
         const eventCollection = client.db("volunteerHelpingHand").collection("events");
+        const volunteerActivityCollection = client.db("volunteerHelpingHand").collection("volunteerActivity");
 
+        // get api
         app.get('/events', async (req, res) => {
             const query = {};
             const cursor = eventCollection.find(query);
             const events = await cursor.toArray();
             res.send(events)
         })
+
+        // post api
+        app.post('/event', async (req, res) => {
+            const body = req.body;
+            const result = await eventCollection.insertOne(body);
+            res.send(result)
+        })
+
+        app.post('/registration', async (req, res) => {
+            const body = req.body;
+            const result = await volunteerActivityCollection.insertOne(body);
+            res.send(result)
+        })
+
+        app.get('/registrationall', async (req, res) => {
+            const query = {};
+            const cursor = volunteerActivityCollection.find(query);
+            const data = await cursor.toArray();
+            res.send(data);
+        })
+        app.get('/registration', async (req, res) => {
+            const email = req.query.email;
+            const query = { email };
+            const cursor = volunteerActivityCollection.find(query);
+            const data = await cursor.toArray();
+            res.send(data);
+        })
+
+        // use post to get products by ids
+        app.post('/eventById', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = { _id: { $in: ids } };
+            const cursor = eventCollection.find(query);
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
     }
     finally {
 
     }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
 
 
